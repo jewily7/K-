18ce76931755404944883e8ed1f16c3bf21b7042
JUWAYLI ABDULLAH <124881484+jewily7@users.noreply.github.com>
2024-06-11 21:55:21 +0300

    Add files via upload

---
+++ js/todo.js
@@ -0,0 +1,31 @@
+
+$(".todo-icon").click(function () {
+	// body...
+	$("input[type=text]").fadeToggle();
+});
+
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("dblclick", "li", function(event){
+	$(this).fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		if($(this).val()==""){
+			alert("Enter a valid ToDo!");
+		}
+		else{
+			$(this).val("");
+			//create a new li and add to ul
+			$("ul.todo-list").append("<li class='todo-item'> " + todoText + "</li>")
+		}
+	}
+});
